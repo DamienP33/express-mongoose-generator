9070c01883f918f46fb72d2b77b0c6983eaa526a
Damien P <perrier.damien@gmail.com>
2014-11-16 17:33:04 +0100

    added the generation Controller

---
+++ templates/controller.js
@@ -0,0 +1,114 @@
+var model = require('../models/{modelName}.js');
+
+/**
+ * {controllerName}.js
+ *
+ * @description :: Server-side logic for managing {pluralName}.
+ */
+module.exports = {
+
+    /**
+     * {controllerName}.list()
+     */
+    list: function(req, res) {
+        model.find(function(err, {pluralName}){
+            if(err) {
+                return res.json(500, {
+                    message: 'Error getting {name}.'
+                });
+            }
+            return res.json({pluralName});
+        });
+    },
+
+    /**
+     * {controllerName}.show()
+     */
+    show: function(req, res) {
+        var id = req.params.id;
+        model.findOne({_id: id}, function(err, {name}){
+            if(err) {
+                return res.json(500, {
+                    message: 'Error getting {name}.'
+                });
+            }
+            if(!{name}) {
+                return res.json(404, {
+                    message: 'No such {name}'
+                });
+            }
+            return res.json({name});
+        });
+    },
+
+    /**
+     * {controllerName}.create()
+     */
+    create: function(req, res) {
+        var {name} = new model({{createFields}
+        });
+
+        {name}.save(function(err, {name}){
+            if(err) {
+                return res.json(500, {
+                    message: 'Error saving {name}',
+                    error: err
+                });
+            }
+            return res.json({
+                message: 'saved',
+                _id: {name}._id
+            });
+        });
+    },
+
+    /**
+     * {controllerName}.update()
+     */
+    update: function(req, res) {
+        var id = req.params.id;
+        model.findOne({_id: id}, function(err, {name}){
+            if(err) {
+                return res.json(500, {
+                    message: 'Error saving {name}',
+                    error: err
+                });
+            }
+            if(!{name}) {
+                return res.json(404, {
+                    message: 'No such {name}'
+                });
+            }
+
+            {updateFields}
+            {name}.save(function(err, {name}){
+                if(err) {
+                    return res.json(500, {
+                        message: 'Error getting {name}.'
+                    });
+                }
+                if(!{name}) {
+                    return res.json(404, {
+                        message: 'No such {name}'
+                    });
+                }
+                return res.json({name});
+            });
+        });
+    },
+
+    /**
+     * {controllerName}.remove()
+     */
+    remove: function(req, res) {
+        var id = req.params.id;
+        model.findByIdAndRemove(id, function(err, {name}){
+            if(err) {
+                return res.json(500, {
+                    message: 'Error getting {name}.'
+                });
+            }
+            return res.json({name});
+        });
+    }
+};
