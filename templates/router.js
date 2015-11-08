0b17bf246c5c2034db6db48c2a84f0de238af1e5
damienp33 <damienperrier33@gmail.com>
2015-11-08 14:42:06 +0100

    change naming controller

--- templates/router.js
+++ templates/router.js
@@ -1,40 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/{controllerName}.js');
+var {controllerName} = require('../controllers/{controllerName}.js');
 
 /*
  * GET
  */
 router.get('/', function(req, res) {
-    controller.list(req, res);
+    {controllerName}.list(req, res);
 });
 
 /*
  * GET
  */
 router.get('/:id', function(req, res) {
-    controller.show(req, res);
+    {controllerName}.show(req, res);
 });
 
 /*
  * POST
  */
 router.post('/', function(req, res) {
-    controller.create(req, res);
+    {controllerName}.create(req, res);
 });
 
 /*
  * PUT
  */
 router.put('/:id', function(req, res) {
-    controller.update(req, res);
+    {controllerName}.update(req, res);
 });
 
 /*
  * DELETE
  */
 router.delete('/:id', function(req, res) {
-    controller.remove(req, res);
+    {controllerName}.remove(req, res);
 });
 
 module.exports = router;
