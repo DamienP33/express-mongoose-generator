6d0e96306345ea80299a309f6753ad9bed98a72e
damienp33 <damienperrier33@gmail.com>
2016-07-19 10:01:20 +0200

    updated response return for compliance

--- templates/controller.js
+++ templates/controller.js
@@ -13,8 +13,9 @@ module.exports = {
     list: function (req, res) {
         {modelName}.find(function (err, {pluralName}) {
             if (err) {
-                return res.json(500, {
-                    message: 'Error getting {name}.'
+                return res.status(500).json({
+                    message: 'Error when getting {name}.',
+                    error: err
                 });
             }
             return res.json({pluralName});
@@ -28,12 +29,13 @@ module.exports = {
         var id = req.params.id;
         {modelName}.findOne({_id: id}, function (err, {name}) {
             if (err) {
-                return res.json(500, {
-                    message: 'Error getting {name}.'
+                return res.status(500).json({
+                    message: 'Error when getting {name}.',
+                    error: err
                 });
             }
             if (!{name}) {
-                return res.json(404, {
+                return res.status(404).json({
                     message: 'No such {name}'
                 });
             }
@@ -50,15 +52,12 @@ module.exports = {
 
         {name}.save(function (err, {name}) {
             if (err) {
-                return res.json(500, {
-                    message: 'Error saving {name}',
+                return res.status(500).json({
+                    message: 'Error when creating {name}',
                     error: err
                 });
             }
-            return res.json({
-                message: 'saved',
-                _id: {name}._id
-            });
+            return res.status(201).json({name});
         });
     },
 
@@ -69,13 +68,13 @@ module.exports = {
         var id = req.params.id;
         {modelName}.findOne({_id: id}, function (err, {name}) {
             if (err) {
-                return res.json(500, {
-                    message: 'Error saving {name}',
+                return res.status(500).json({
+                    message: 'Error when getting {name}',
                     error: err
                 });
             }
             if (!{name}) {
-                return res.json(404, {
+                return res.status(404).json({
                     message: 'No such {name}'
                 });
             }
@@ -83,15 +82,12 @@ module.exports = {
             {updateFields}
             {name}.save(function (err, {name}) {
                 if (err) {
-                    return res.json(500, {
-                        message: 'Error getting {name}.'
+                    return res.status(500).json({
+                        message: 'Error when updating {name}.',
+                        error: err
                     });
                 }
-                if (!{name}) {
-                    return res.json(404, {
-                        message: 'No such {name}'
-                    });
-                }
+
                 return res.json({name});
             });
         });
@@ -104,11 +100,12 @@ module.exports = {
         var id = req.params.id;
         {modelName}.findByIdAndRemove(id, function (err, {name}) {
             if (err) {
-                return res.json(500, {
-                    message: 'Error getting {name}.'
+                return res.status(500).json({
+                    message: 'Error when deleting the {name}.',
+                    error: err
                 });
             }
-            return res.json({name});
+            return res.status(204).json();
         });
     }
 };
