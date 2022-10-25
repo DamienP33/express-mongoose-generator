b8a8d61de1a3d05210a1b648c039273bda31d6bb
BetweenTwoTests <betweentwotests@gmail.com>
2022-10-25 15:31:28 -0400

    Port over changes from https://github.com/DamienP33/express-mongoose-generator/pull/23

--- templates/model.js
+++ templates/model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-const {schemaName} = new Schema({fields});
+const {schemaName} = new Schema(
+        {fields},
+        {
+            timestamps: true,
+        }
+    );
 
 module.exports = mongoose.model('{modelName}', {schemaName});
